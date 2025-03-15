23a5f549f67489bdc14325940ffbc02406184e55
Kdeeja_Mstafa <118957732+KhadeejaMustafa@users.noreply.github.com>
2025-03-16 00:28:58 +0300

    Update main.py
    adding background
    adding keyboard listener

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ import pyautogui # used to monitor mouse activity.
 import tkinter # package to create pop up window
 import time
 from PIL import Image, ImageTk # Python Imaging Library or Pillow. Used for opening and displaying images.
+import keyboard # to handle termination of program through escape key
 
 def show_motivation():
     root = tkinter.Tk() # used to initialize the main window
     root.title("Keep Going!")
-    canvas = tkinter.Canvas(root, width= 600, height= 400, bg= 'light blue', 
-                            highlightbackground='rgb(0, 102, 204)', highlightthickness= 2)
-    canvas.pack()
 
-    Img = ImageTk.PhotoImage(Image.open("https://i.pinimg.com/736x/47/c7/92/47c79270a9a671ce17c0257f617c18d0.jpg"))
-    canvas.create_image(200, 150, image = Img)
+    # creating a background image for the canvas
+    bg_image = Image.open("bg_pattern.jpg")
+    bg_image = bg_image.resize((750, 1300))
+    background_img = ImageTk.PhotoImage(bg_image)
+
+    # creates Canvas
+    canvas = tkinter.Canvas(root, width= 750, height= 1300, highlightbackground='brown', highlightthickness= 4)
+    canvas.pack()
+    # adding the background image
+    canvas.create_image(0, 0, image = background_img, anchor= tkinter.NW)
+    
+
+    Img = ImageTk.PhotoImage(Image.open("cameraimg.jpg"))
+    canvas.create_image(500, 500, image = Img)
 
     canvas.create_text(200, 50, text= "Don't give up yet, remember you wanted to by a DSLR camera?", font=("Helvetica", 24))
 
@@ -24,8 +34,13 @@ def mouse_inactivity():
     last_position = pyautogui.position() # Gets the current position of the mouse.
     inactivity_limit = 5 # seconds
 
-    while true:
-        time.sleep() # will pause the script for one second before checking the mouse position again
+    while True:
+
+        if keyboard.is_pressed("esc"):
+            print("Exiting the program..")
+            break
+
+        time.sleep(1) # will pause the script for one second before checking the mouse position again
         current_position = pyautogui.position() # Gets the current position of the mouse.
         if last_position == current_position: # if these mouse position has not changed, then turn the inactivity_limit into a value smaller than zero
             inactivity_limit -= 1
@@ -37,3 +52,4 @@ def mouse_inactivity():
             inactivity_limit = 5
 
 mouse_inactivity()
+
